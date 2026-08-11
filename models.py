41798fd9d66ecaffe9458d17a9c6442f6f9a22fd
Zulqarnai <mzulqarnainceh@gmail.com>
2026-08-11 12:47:45 +0500

    added code of database creation and steup

--- models.py
+++ models.py
@@ -3,6 +3,40 @@ folder=os.path.dirname(__file__)
 path=os.path.join(folder,"database.db")
 # database setup
 def get_db_connection():
-    pass
+    conn=sqlite3.connect(path)
+    conn.execute("PRAGMA foreign_keys = ON")
+    conn.row_factory=sqlite3.Row
+    return conn
 def init_db():
-    pass
+    conn=get_db_connection()
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
+    )
+    """)
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS posts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT,
+    user_id INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+    """)
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS comments(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_id INTEGER,
+    user_id INTEGER,
+    comment_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+    """)
+    conn.commit()
+    conn.close()
