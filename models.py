19b757c4b7754cbe3141cff88727c463fa6b125f
Zulqarnai <mzulqarnainceh@gmail.com>
2026-08-14 19:30:43 +0500

    updated comments table (on delete cascade), if post deleted the comments should also be delete

--- models.py
+++ models.py
@@ -34,7 +34,7 @@ def init_db():
     user_id INTEGER,
     comment_text TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id)
     )
     """)
